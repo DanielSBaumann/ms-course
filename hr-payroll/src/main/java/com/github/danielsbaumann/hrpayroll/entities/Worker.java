de18e4db3d8692223eefac0f3cabf8a16e2c9c5a
DanielSBaumann <dr4wone@gmail.com>
2021-06-01 16:02:08 -0300

    rest template

---
+++ hr-payroll/src/main/java/com/github/danielsbaumann/hrpayroll/entities/Worker.java
@@ -0,0 +1,29 @@
+package com.github.danielsbaumann.hrpayroll.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker {
+    private Long id;
+    private String name;
+    private double dailyIncome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(id, worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
