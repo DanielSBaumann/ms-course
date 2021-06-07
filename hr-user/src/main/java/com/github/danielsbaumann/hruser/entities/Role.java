ae797cfe3146fa8c8a076899414efa8eded432f6
DanielSBaumann <dr4wone@gmail.com>
2021-06-07 17:24:42 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/github/danielsbaumann/hruser/entities/Role.java
@@ -0,0 +1,35 @@
+package com.github.danielsbaumann.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "tb_role")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role implements Serializable {
+    private static final Long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleName, role.roleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
+    }
+}
---
+++ hr-user/src/main/java/com/github/danielsbaumann/hruser/entities/User.java
@@ -0,0 +1,48 @@
+package com.github.danielsbaumann.hruser.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "tb_user")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
